a26214c382a273c70728fa873dd72cf18571899d
Paddy Mullen <paddy@paddymullen.com>
2025-04-03 19:04:58 -0400

    WIP

--- js/widget.tsx
+++ js/widget.tsx
@@ -5,12 +5,15 @@ import "./widget.css";
 const render = createRender(() => {
 	const [value, setValue] = useModelState<number>("value");
 	return (
-		<div className="anywidget_styling">
-			<button onClick={() => setValue(value + 1)}>
-				count is {value}
-			</button>
-		</div>
-	);
+	    <div style={{height:500, width:800, border:"1px solid red"}} className={"ag-theme-alpine-dark"}>
+                <span class="ag-sort-indicator-container" style={{color:"green", height:50, width:50, border:"3px solid orange" }}  data-ref="eSortIndicator">
+                    <span data-ref="eSortOrder" class="ag-sort-indicator-icon ag-sort-order ag-hidden" aria-hidden="true">1</span>
+                    <span data-ref="eSortAsc" class="ag-sort-indicator-icon ag-sort-ascending-icon" aria-hidden="true">
+                    <span class="ag-icon ag-icon-asc" style={{color:"black"}} unselectable="on" role="presentation"></span>
+                </span>
+	  </span>
+	  <span class="ag-icon ag-icon-asc" unselectable="on" role="presentation"></span>
+     </div>);
 });
 
 export default { render };
